f7769af8159d2c76826aec362f4c98e966f21911
Markus Tacker <m@coderbyheart.de>
2013-05-03 18:44:22 +0200

    Print E-Mails only in verbose mode.

--- src/WEMOOF/BackendBundle/Command/SendConfirmationsCommand.php
+++ src/WEMOOF/BackendBundle/Command/SendConfirmationsCommand.php
@@ -2,6 +2,10 @@
 
 namespace WEMOOF\BackendBundle\Command;
 
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use WEMOOF\BackendBundle\Value\SchemeAndHostValue;
+
 class SendConfirmationsCommand extends \Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand
 {
     protected function configure()
@@ -11,15 +15,15 @@ class SendConfirmationsCommand extends \Symfony\Bundle\FrameworkBundle\Command\C
             ->setDescription('Send registration confirmation emails');
     }
 
-    protected function execute(\Symfony\Component\Console\Input\InputInterface $input, \Symfony\Component\Console\Output\OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output)
     {
         /** @var \WEMOOF\BackendBundle\Repository\RegistrationRepositoryInterface $repo */
         $repo = $this->getContainer()->get('wemoof.backend.repo.registration');
         /** @var \LiteCQRS\Bus\CommandBus $commandBus */
         $commandBus = $this->getContainer()->get('command_bus');
         foreach ($repo->getUnconfirmedRegistrations() as $registration) {
-            $output->writeln($registration->getUser()->getEmail());
-            $commandBus->handle(SendConfirmationMailCommand::create($registration, \WEMOOF\BackendBundle\Value\SchemeAndHostValue::parse($this->getContainer()->getParameter("scheme_and_host"))));
+            if ($output->getVerbosity() === OutputInterface::VERBOSITY_VERBOSE) $output->writeln($registration->getUser()->getEmail());
+            $commandBus->handle(SendConfirmationMailCommand::create($registration, SchemeAndHostValue::parse($this->getContainer()->getParameter("scheme_and_host"))));
         }
     }
 }
