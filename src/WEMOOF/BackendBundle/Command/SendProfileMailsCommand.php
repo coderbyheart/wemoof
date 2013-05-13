3faa78280b6588121b2659578c9ae4423f5aafca
Markus Tacker <m@coderbyheart.de>
2013-05-13 14:08:02 +0200

    Parameter after zum Reminder dazu.

--- src/WEMOOF/BackendBundle/Command/SendProfileMailsCommand.php
+++ src/WEMOOF/BackendBundle/Command/SendProfileMailsCommand.php
@@ -2,6 +2,7 @@
 
 namespace WEMOOF\BackendBundle\Command;
 
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputInterface;
 use WEMOOF\BackendBundle\Value\SchemeAndHostValue;
@@ -12,7 +13,8 @@ class SendProfileMailsCommand extends \Symfony\Bundle\FrameworkBundle\Command\Co
     {
         $this
             ->setName('wemoof:sendprofilemails')
-            ->setDescription('Send profile reminder emails');
+            ->setDescription('Send profile reminder emails')
+            ->addArgument('after', InputArgument::OPTIONAL, 'Only send reminders to users registered after this date');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -21,7 +23,12 @@ class SendProfileMailsCommand extends \Symfony\Bundle\FrameworkBundle\Command\Co
         $repo = $this->getContainer()->get('wemoof.backend.repo.user');
         /** @var \LiteCQRS\Bus\CommandBus $commandBus */
         $commandBus = $this->getContainer()->get('command_bus');
+        $after      = $input->getArgument('after') ? new \DateTime($input->getArgument('after')) : null;
         foreach ($repo->getUsersWithoutPublicProfile() as $user) {
+            if ($after !== null && $after->diff($user->getCreated())->invert > 0) {
+                if ($output->getVerbosity() === OutputInterface::VERBOSITY_VERBOSE) $output->writeln(sprintf('Skipping %s', $user->getEmail()));
+                continue;
+            }
             if ($output->getVerbosity() === OutputInterface::VERBOSITY_VERBOSE) $output->writeln($user->getEmail());
             $commandBus->handle(SendProfileMailCommand::create($user, SchemeAndHostValue::parse($this->getContainer()->getParameter("scheme_and_host"))));
         }
