3f39254493b0a64353674f1203ef70395882f991
Markus Tacker <m@coderbyheart.de>
2013-09-02 13:41:07 +0200

    WIP.

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,12 +1,9 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
 
 setlocale(LC_ALL, getenv('LOCALE'));
-
-// If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
-//umask(0000);
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
@@ -19,6 +16,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 }
 
 $loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
+Debug::enable();
 require_once __DIR__ . '/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
