e590a5065e75b58475f0df40261e9c8b232dcfdf
Markus Tacker <m@coderbyheart.de>
2013-11-17 16:54:19 +0100

    Replace multiple occurrences of - in slugs.

--- src/WEMOOF/WebBundle/Slugger.php
+++ src/WEMOOF/WebBundle/Slugger.php
@@ -15,6 +15,7 @@ class Slugger
         $str = str_replace(' ', '-', $str);
         $str = iconv("UTF-8", "ASCII//TRANSLIT", $str);
         $str = preg_replace('/[^a-z0-9-]/', '', $str);
+        $str = preg_replace('/-{2,}/', '-', $str);
         return $str;
     }
 }
